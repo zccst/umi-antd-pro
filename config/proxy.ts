b79fb0be74b813521506ed5dcc7ba25eb339696c
zccst <zccst@qq.com>
2023-09-14 22:27:07 +0800

    add modal in abi manage

--- config/proxy.ts
+++ config/proxy.ts
@@ -7,16 +7,16 @@
  * https://pro.ant.design/docs/deploy
  */
 export default {
-  // dev: {
-  //   // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
-  //   '/api/': {
-  //     // 要代理的地址
-  //     target: 'https://preview.pro.ant.design',
-  //     // 配置了这个可以从 http 代理到 https
-  //     // 依赖 origin 的功能可能需要这个，比如 cookie
-  //     changeOrigin: true,
-  //   },
-  // },
+  dev: {
+    // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
+    '/api/': {
+      // 要代理的地址
+      target: 'http://44.238.134.212:8020',
+      // 配置了这个可以从 http 代理到 https
+      // 依赖 origin 的功能可能需要这个，比如 cookie
+      changeOrigin: true,
+    },
+  },
   // test: {
   //   '/api/': {
   //     target: 'https://proapi.azurewebsites.net',
--- src/pages/contract/call/abi.tsx
+++ src/pages/contract/call/abi.tsx
@@ -1,6 +1,6 @@
 import { ActionType, ProColumns } from '@ant-design/pro-components';
 import { GridContent, ProTable, TableDropdown } from '@ant-design/pro-components';
-import { Button, Dropdown, Space, Tag } from 'antd';
+import { Button, Form, Input, Modal, Radio, Tag } from 'antd';
 import { EllipsisOutlined, PlusOutlined } from '@ant-design/icons';
 import { Suspense, useState, useEffect, useRef } from 'react';
 import request from 'umi-request';
@@ -8,179 +8,283 @@ import PageLoading from '../components/PageLoading';
 import { shortenAddress } from '../../../utils/utils';
 
 
-export const waitTimePromise = async (time: number = 100) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(true);
-      }, time);
-    });
-};
-export const waitTime = async (time: number = 100) => {
-    await waitTimePromise(time);
-};
-  
-type GithubIssueItem = {
-    url: string;
+
+type AbiItem = {
     id: number;
-    number: number;
-    title: string;
-    labels: {
-      name: string;
-      color: string;
-    }[];
+    name: string;
+    version: number;
+    env: string;
     state: string;
-    comments: number;
-    created_at: string;
-    updated_at: string;
-    closed_at?: string;
+    department_id: number;
+    department_name: string;
+    project_id: number;
+    project_name: string;
+    last_edit_user_id: number;
+    last_edit_user_name: string;
+    create_time: string;
+    update_time: string;
+    latest: boolean;
+    updatable: boolean;
+    addrs: [];
+    abi: string;
 };
-  
-const columns: ProColumns<GithubIssueItem>[] = [
-    {
-      dataIndex: 'index',
-      valueType: 'indexBorder',
-      width: 48,
-    },
-    {
-      title: '标题',
-      dataIndex: 'title',
-      copyable: true,
-      ellipsis: true,
-      tip: '标题过长会自动收缩',
-      // 传递给 Form.Item 的配置
-      formItemProps: {
-        rules: [
-          {
-            required: true,
-            message: '此项为必填项',
-          },
-        ],
-      },
-    },
-    {
-      disable: true, // 列设置中disabled的状态
-      title: '状态',
-      dataIndex: 'state',
-      filters: true, // 表头的筛选菜单项，当值为 true 时，自动使用 valueEnum 生成
-      onFilter: true,// 筛选表单，为 true 时使用 ProTable 自带的，为 false 时关闭本地筛选
-      ellipsis: true,
-      valueType: 'select',
-      valueEnum: {
-        all: { text: '超长'.repeat(50) },
-        open: {
-          text: '未解决',
-          status: 'Error',
-        },
-        closed: {
-          text: '已解决',
-          status: 'Success',
-          disabled: true,
-        },
-        processing: {
-          text: '解决中',
-          status: 'Processing',
-        },
-      },
-    },
-    {
-      disable: true, // 列设置中disabled的状态
-      title: '标签',
-      dataIndex: 'labels',
-      search: false, // 配置列的搜索相关，false 为隐藏
-      // 渲染查询表单的输入组件
-      renderFormItem: (_, { defaultRender }) => {
-        return defaultRender(_);
-      },
-      render: (_, record) => (
-        <Space>
-          {record.labels.map(({ name, color }) => (
-            <Tag color={color} key={name}>
-              {name}
-            </Tag>
-          ))}
-        </Space>
-      ),
-    },
-    {
-      title: '创建时间',
-      key: 'showTime',
-      dataIndex: 'created_at',
-      valueType: 'date',
-      sorter: true,
-      hideInSearch: true, // 在查询表单中不展示此项
-    },
-    {
-      title: '创建时间',
-      dataIndex: 'created_at',
-      valueType: 'dateRange',
-      hideInTable: true, // 在 Table 中不展示此列
-      search: { // 配置列的搜索相关，false 为隐藏
-        // 转化值的 key, 一般用于时间区间的转化
-        transform: (value) => {
-          return {
-            startTime: value[0],
-            endTime: value[1],
-          };
-        },
-      },
-    },
-    {
-      title: '操作',
-      valueType: 'option',
-      key: 'option',
-      render: (text, record, _, action) => [
-        <a
-          key="editable"
-          onClick={() => {
-            action?.startEditable?.(record.id);
-          }}
+
+interface Values {
+  title: string;
+  description: string;
+  modifier: string;
+}
+
+interface CollectionCreateFormProps {
+  open: boolean;
+  onCreate: (values: Values) => void;
+  onCancel: () => void;
+}
+
+const CollectionCreateForm: React.FC<CollectionCreateFormProps> = ({
+  open,
+  onCreate,
+  onCancel,
+}) => {
+  const [form] = Form.useForm();
+  return (
+    <Modal
+      open={open}
+      title="Create a new collection"
+      okText="Create"
+      cancelText="Cancel"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch((info) => {
+            console.log('Validate Failed:', info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{ modifier: 'public' }}
+      >
+        <Form.Item
+          name="title"
+          label="Title"
+          rules={[{ required: true, message: 'Please input the title of collection!' }]}
         >
-          编辑
-        </a>,
-        <a href={record.url} target="_blank" rel="noopener noreferrer" key="view">
-          查看
-        </a>,
-        <TableDropdown
-          key="actionGroup"
-          onSelect={() => action?.reload()}
-          menus={[
-            { key: 'copy', name: '复制' },
-            { key: 'delete', name: '删除' },
-          ]}
-        />,
-      ],
-    },
-];
+          <Input />
+        </Form.Item>
+        <Form.Item name="description" label="Description">
+          <Input type="textarea" />
+        </Form.Item>
+        <Form.Item name="modifier" className="collection-create-form_last-form-item">
+          <Radio.Group>
+            <Radio value="public">Public</Radio>
+            <Radio value="private">Private</Radio>
+          </Radio.Group>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
 
 
 const Abi: React.FC = () => {
     const actionRef = useRef<ActionType>();
 
+    const [form] = Form.useForm();
 
-    const deptHandleChange = (value: string) => {
-        console.log(`selected ${value}`);
+    const [open, setOpen] = useState(false);
+    const [confirmLoading, setConfirmLoading] = useState(false);
+    const [modalItem, setModalItem] = useState<{[key: string]: any}>({});
+    // const [modalItem, setModalItem] = useState({});
+
+    // 修改
+    const showModal = (item: any) => {
+      setOpen(true);
+      setModalItem(item);
+      console.log('showModal', item);
     };
-    const projectHandleChange = (value: string) => {
-        console.log(`selected ${value}`);
+    const handleOk = () => {
+      setConfirmLoading(true);
+      setTimeout(() => {
+        setOpen(false);
+        setConfirmLoading(false);
+      }, 2000);
     };
-    const envHandleChange = (value: string) => {
-        console.log(`selected ${value}`);
+    const handleCancel = () => {
+      console.log('Clicked cancel button');
+      setOpen(false);
     };
+
+    // 表格的列
+    const columns: ProColumns<AbiItem>[] = [
+      {
+        title: '名称',
+        dataIndex: 'name',
+        copyable: true,
+        ellipsis: true,
+        tip: '名称过长会自动收缩',
+        // 传递给 Form.Item 的配置
+        formItemProps: {
+          rules: [
+            {
+              required: true,
+              message: '此项为必填项',
+            },
+          ],
+        },
+      },
+      {
+        title: '版本',
+        key: 'version',
+        dataIndex: 'version',
+        hideInSearch: true, // 在查询表单中不展示此项
+      },
+      {
+        title: '已关联地址',
+        key: 'addrs',
+        dataIndex: 'addrs',
+        hideInSearch: true, // 在查询表单中不展示此项
+      },
+      {
+        title: '环境',
+        key: 'env',
+        dataIndex: 'env',
+        valueType: 'select',
+        valueEnum: {
+          all: {text: '全部'},
+          available: {
+            text: 'prod',
+          },
+          disable: {
+            text: 'test',
+          },
+        },
+      },
+      {
+        disable: true, // 列设置中disabled的状态
+        title: '状态',
+        dataIndex: 'state',
+        filters: true, // 表头的筛选菜单项，当值为 true 时，自动使用 valueEnum 生成
+        onFilter: true,// 筛选表单，为 true 时使用 ProTable 自带的，为 false 时关闭本地筛选
+        ellipsis: true,
+        valueType: 'select',
+        valueEnum: {
+          all: {text: '全部'},
+          available: {
+            text: '正常',
+            status: 'Success',
+          },
+          disable: {
+            text: '不可用',
+            status: 'Error',
+          },
+        },
+      },
+      {
+        title: '部门',
+        key: 'department_name',
+        dataIndex: 'department_name',
+        valueType: 'select',
+      },
+      {
+        title: '项目',
+        key: 'project_name',
+        dataIndex: 'project_name',
+        valueType: 'select',
+      },
+      {
+        title: '最后修改时间',
+        key: 'update_time',
+        dataIndex: 'update_time',
+        valueType: 'date',
+        sorter: true,
+        hideInSearch: true, // 在查询表单中不展示此项
+      },
+      {
+        title: '最后修改时间',
+        dataIndex: 'update_time',
+        valueType: 'dateRange',
+        hideInTable: true, // 在 Table 中不展示此列
+        search: { // 配置列的搜索相关，false 为隐藏
+          // 转化值的 key, 一般用于时间区间的转化
+          transform: (value) => {
+            return {
+              startTime: value[0],
+              endTime: value[1],
+            };
+          },
+        },
+      },
+      {
+        title: '操作',
+        valueType: 'option',
+        key: 'option',
+        render: (text, record, _, action) => [
+          <a
+            key="editable"
+            onClick={() => {
+              console.log('edit', record);
+              // action?.startEditable?.(record.id);
+              showModal(record);
+            }}
+          >
+            编辑
+          </a>,
+          <a href={'https://www.baidu.com'} target="_blank" rel="noopener noreferrer" key="view">
+            查看
+          </a>,
+          <TableDropdown
+            key="actionGroup"
+            onSelect={() => action?.reload()}
+            menus={[
+              { key: 'copy', name: '复制' },
+              { key: 'delete', name: '删除' },
+            ]}
+          />,
+        ],
+      },
+    ];
+    console.log('modalItem.env', modalItem?.env);
 
     return <GridContent>
         <>
-            <ProTable<GithubIssueItem>
+            <ProTable<AbiItem>
                 columns={columns}
                 actionRef={actionRef} // Table action 的引用，便于自定义触发
                 cardBordered  // Table 和 Search 外围 Card 组件的边框
                 request={async (params = {}, sort, filter) => {
-                    console.log('params', sort, filter);
-                    // await waitTime();
-                    return request<{
-                        data: GithubIssueItem[];
-                    }>('https://proapi.azurewebsites.net/github/issues', {
-                        params,
-                    });
+                    console.log('params', params, sort, filter);
+                    params = {
+                      ...params,
+                      page: params.current,
+                      size: params.pageSize,
+                    };
+                    console.log('params', params);
+                    let ret: any = {};
+                    await request<{
+                        data: AbiItem[];
+                    }>('http://44.238.134.212:8020/api/v1/abi/list', {
+                      params,
+                    }).then(r => ret = r);
+                    console.log('ret', ret, typeof ret);
+                    const res = {
+                      data: ret.data.list,
+                      // success 请返回 true，
+                      // 不然 table 会停止解析数据，即使有数据
+                      success: ret.code === 0 ? true: false,
+                      // 不传会使用 data 的长度，如果是分页一定要传
+                      total: ret.data.total,
+                    }
+
+                    return Promise.resolve(res);
                 }}
                 editable={{
                     type: 'multiple',
@@ -195,6 +299,8 @@ const Abi: React.FC = () => {
                 rowKey="id"
                 search={{ // 是否显示搜索表单，传入对象时为搜索表单的配置
                     labelWidth: 'auto', // 标签的宽度 'number' | 'auto'
+                    // collapsed: false, // 默认展开状态并去掉"收起"选项
+                    // collapseRender:() => null
                 }}
                 options={{ // table 工具栏，设为 false 时不显示.传入 function 会点击时触发
                     setting: {
@@ -235,6 +341,48 @@ const Abi: React.FC = () => {
                     </Button>,
                 ]}
                 />
+
+            <Modal
+              title="Title"
+              open={open}
+              onOk={handleOk}
+              confirmLoading={confirmLoading}
+              onCancel={handleCancel}
+            >
+              <Form
+                form={form}
+                name="form_in_modal"
+                initialValues={{
+                  ...modalItem ,
+                  updatable: modalItem?.updatable ? 'true' : "false"
+                }
+                }
+              >
+                <Form.Item
+                  name="title"
+                  label="Title"
+                  rules={[{ required: true, message: 'Please input the title of collection!' }]}
+                >
+                  <Input />
+                </Form.Item>
+                <Form.Item name="description" label="Description">
+                  <Input type="textarea" />
+                </Form.Item>
+                <Form.Item name="updatable" label="abi类型">
+                  <Radio.Group>
+                    <Radio value="true">可升级</Radio>
+                    <Radio value="false">不可升级</Radio>
+                  </Radio.Group>
+                </Form.Item>
+                <Form.Item name="env" label="所属环境">
+                  <Radio.Group>
+                    <Radio value="prod">prod</Radio>
+                    <Radio value="test">test</Radio>
+                  </Radio.Group>
+                </Form.Item>
+                <p>{modalItem.env}</p>
+              </Form>
+            </Modal>
         </>
     </GridContent>;
 };
